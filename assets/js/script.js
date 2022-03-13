7903458022035d2c31711fc3e6cc0bd5e41d0518
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-03-13 00:07:37 -0600

    added timeblock html + css + adjusted hour div css + added midnight class + added 24 time blocks to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+//Call a var for the moment format we desire
 var present= moment().format('MMMM Do YYYY, h:mm:ss a')
-
+//append the var to html id for currentDay
 $('#currentDay').html(present)
+
 
 
