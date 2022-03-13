8fde28dd5a97871366839df21e8caa0a2153fa94
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-03-13 11:40:15 -0500

    create thisTime(){currentTime} with moment, Jqueried each timeBlock and parsedInt to match currentTime, added past present future classes to html dependent on timeBlock ~==~ currentTime

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,53 @@ var present= moment().format('MMMM Do YYYY, h:mm:ss a')
 //append the var to html id for currentDay
 $('#currentDay').html(present)
 
+//check time
+function thisTime() {
+    //current time variable
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+    var timeBlock = parseInt($(this).attr("id"), 10 , 1);
+        
+    //apply background colors depending on thisTime
+
+    if (timeBlock < currentTime) {
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+        $(this).addClass('past');
+    } else if (timeBlock === currentTime) {
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+    } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+    }
+});
+}
+
+thisTime()
 
 
+
+// TRASH CODE DO NOT UNCOMMENT
+//Gets the current hour in HH military format
+//var thisTime = moment().format('HH');
+//console.log(thisTime)
+//array of I for current hour
+//taskArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+//Future element IDs of EACH time-block
+//const timeBlock0 = document.getElementById("0");
+//console.log(timeBlock)
+//function myFunction() {
+//    var taskBox0 = document
+//}
+//future proofing for loop time logic
+//pastTime = timeBlock < thisTime;
+//presentTime = (timeBlock === thisTime);
+//futureTime = timeBlock > thisTime;
+//for (let i = 0 ; i < taskArray.length ; i++ ) { 
+//};
+//timeBlock = [i]
+//var pastTime = thisTime >
